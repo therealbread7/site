d2a36cbff643dd42c21d89ea255be80b5def1bc7
therealbread7 <143816748+therealbread7@users.noreply.github.com>
2026-08-22 00:38:51 +0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import GiftVault from "./GiftVault.jsx";
+createRoot(document.getElementById("root")).render(<GiftVault />);
